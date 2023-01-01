bb21c98205fb54a2d2b0b5ef03a70a15ad9cec8e
mohaphez <mohaphez@yahoo.com>
2023-01-02 01:33:31 +0330

    fix: all tests passed without error

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,11 @@
 <?php
 
 use dnj\Ticket\Http\Controllers\DepartmentController;
+use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Support\Facades\Route;
 
-Route::resource('departments', DepartmentController::class);
+Route::middleware([SubstituteBindings::class])->group(
+    function () {
+        Route::resource('departments', DepartmentController::class);
+    }
+);
