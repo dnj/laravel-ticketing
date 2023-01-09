30e6ff7a35c1b40880addf1ee744c875a9190b46
mohaphez <mohaphez@yahoo.com>
2023-01-09 19:24:52 +0330

    fix: update routes file

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use dnj\Ticket\Http\Controllers\DepartmentController;
 use dnj\Ticket\Http\Controllers\TicketController;
 use dnj\Ticket\Http\Controllers\TicketMessageController;
+use dnj\Ticket\Http\Controllers\TicketAttachmentController;
 use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Support\Facades\Route;
 
@@ -12,6 +13,7 @@ Route::middleware([SubstituteBindings::class, 'auth'])->group(
             'departments' => DepartmentController::class,
             'tickets' => TicketController::class,
             'tickets.messages' => TicketMessageController::class,
+            'ticketAttachments' => TicketAttachmentController::class,
         ]);
     }
 );
